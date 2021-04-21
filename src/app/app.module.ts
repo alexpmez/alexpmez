61125140f0b7258ff8d9646705ead9e01403f801
qbacreative <qbacreative@gmail.com>
2021-04-21 01:04:23 -0400

    app module working

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,36 +1,51 @@
-import { NgModule } from '@angular/core';
+import { ImageFilterPipe } from './work/filter.pipe';
 import { BrowserModule } from '@angular/platform-browser';
+import { NgModule, enableProdMode } from '@angular/core';
+import { ReactiveFormsModule } from '@angular/forms';
+//import { AngularFireModule } from 'angularfire2';
+//import { AngularFireDatabaseModule } from 'angularfire2/database';
+import { environment } from './../environments/environment';
+import { HashLocationStrategy, LocationStrategy } from '@angular/common';
 
 import { AppRoutingModule } from './app-routing.module';
+
 import { AppComponent } from './app.component';
-import { ContactComponent } from './contact/contact.component';
 import { HeaderComponent } from './header/header.component';
-import { ResumeComponent } from './resume/resume.component';
 import { FooterComponent } from './footer/footer.component';
-import { TitlesComponent } from './titles/titles.component';
 import { WorkComponent } from './work/work.component';
+import { ProjectDetailComponent } from './work/project-detail/project-detail.component';
+import { ResumeComponent } from './resume/resume.component';
+import { ContactComponent } from './contact/contact.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { TitlesComponent } from './titles/titles.component';
 import { SkillsComponent } from './resume/skills/skills.component';
 import { ProfileComponent } from './resume/profile/profile.component';
-import { ProjectDetailComponent } from './work/project-detail/project-detail.component';
+
+enableProdMode();
 
 @NgModule({
   declarations: [
     AppComponent,
-    ContactComponent,
     HeaderComponent,
-    ResumeComponent,
     FooterComponent,
-    TitlesComponent,
     WorkComponent,
+    ProjectDetailComponent,
+    ImageFilterPipe,
+    ResumeComponent,
+    ContactComponent,
+    TitlesComponent,
     SkillsComponent,
-    ProfileComponent,
-    ProjectDetailComponent
+    ProfileComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    ReactiveFormsModule,
+    //AngularFireModule.initializeApp(environment.firebase),
+    //AngularFireDatabaseModule,
+    BrowserAnimationsModule
   ],
-  providers: [],
+  providers: [{provide: LocationStrategy, useClass: HashLocationStrategy}],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
